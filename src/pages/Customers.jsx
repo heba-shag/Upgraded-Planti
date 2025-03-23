cb527f9eccff78361bc9ca86083ac23b05593a59
heba-shag <he.shag5007@gmail.com>
2025-03-23 03:30:22 +0300

    Setting Up cusomers page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
-
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { customersData, customersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Customers = () => {
-  const selectionsettings = { persistSelection: true };
-  const toolbarOptions = ['Delete'];
-  const editing = { allowDeleting: true, allowEditing: true };
+  // تحويل البيانات إلى تنسيق يتوافق مع MUI DataGrid
+  console.log(customersGrid.field)
+  const columns = customersGrid.map((item) => ({
+    field: item.field,
+    headerName: item.headerText,
+    width: item.width,
+    editable: item.editable,
+  }));
+
+  const rows = customersData.map((item, index) => ({
+    id: index,
+    ...item,
+  }));
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Customers" />
-      <GridComponent
-        dataSource={customersData}
-        enableHover={false}
-        allowPaging
-        pageSettings={{ pageCount: 5 }}
-        selectionSettings={selectionsettings}
-        toolbar={toolbarOptions}
-        editSettings={editing}
-        allowSorting
-      >
-        <ColumnsDirective>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-        </ColumnsDirective>
-        <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
-      </GridComponent>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+          components={{
+            Toolbar: GridToolbar,
+          }}
+          editMode="row"
+          experimentalFeatures={{ newEditingApi: true }}
+        />
+      </div>
     </div>
   );
 };
